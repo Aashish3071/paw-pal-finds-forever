4dc343163fe244d69325970ca5c46b8f6fc3f685
Aashish Sharma <67496682+Aashish3071@users.noreply.github.com>
2025-12-24 13:58:43 +0530

    Delete supabase directory

--- supabase/migrations/20250722173947-eeaf6c0b-9711-4155-9889-8a831784bc15.sql
+++
@@ -1,9 +0,0 @@
--- Fix function security by setting search_path
-CREATE OR REPLACE FUNCTION public.handle_new_user()
-RETURNS trigger AS $$
-BEGIN
-  INSERT INTO public.users (id, name)
-  VALUES (new.id, new.raw_user_meta_data->>'name');
-  RETURN new;
-END;
-$$ LANGUAGE plpgsql SECURITY DEFINER SET search_path = '';
